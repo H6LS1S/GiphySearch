05f73396d15afc63be6a28d40d84c3356da6c2b7
Helsis <seagull.bogdan@gmail.com>
2019-07-14 08:36:25 +0300

    feat ( add custom field components )

---
+++ client/components/inputs/TextField.vue
@@ -0,0 +1,33 @@
+<template>
+  <ValidationProvider
+    :vid="vid"
+    :name="$attrs.label"
+    :rules="rules"
+  >
+    <v-text-field
+      v-model="value"
+      v-bind="$attrs"
+      v-on="$listeners"
+      :error-messages="errors"
+      :success="valid"
+      slot-scope="{ errors, valid }"
+    />
+  </ValidationProvider>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { ValidationProvider } from 'vee-validate';
+
+@Component({
+  components: {
+    ValidationProvider,
+  },
+})
+export default class TextField extends Vue {
+  @Prop({ type: String, required: true }) rules: string;
+  @Prop({ type: String, required: false }) vid: string;
+
+  value: string = '';
+}
+</script>
