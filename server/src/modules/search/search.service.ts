c64da880abea50cb877243039c7e66982c9e8bb5
Helsis <seagull.bogdan@gmail.com>
2019-07-17 22:44:28 +0300

    chore ( refactoring ): search service

--- server/src/modules/search/search.service.ts
+++ server/src/modules/search/search.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import * as giphy from 'giphy-api';
 
 import { ConfigService } from '../../config/config.service';
 
@@ -11,25 +12,29 @@ export class SearchService {
     private readonly likesService: LikesService,
   ) {}
 
-  private readonly giphy = require('giphy-api')(
+  private readonly giphy = giphy(
     this.configService.getSetting('GIPHY_API_TOKEN'),
   );
 
-  async selectTrending(): Promise<[]> {
-    const { data } = await this.giphy.trending();
-    return data;
-  }
-
-  async selectIds(scope: [string] | string[]): Promise<[]> {
-    const { data } = await this.giphy.id(scope);
-    return data;
-  }
-
-  async selectByTag(tag: string): Promise<[]> {
-    let { data } = await this.giphy.search(tag);
+  async comparisonWithLikes(data): Promise<any> {
     for await (let image of data) {
       image.likes = await this.likesService.selectByImage(image.id);
     }
     return data;
+  }
+
+  async selectTrending(): Promise<[]> {
+    const { data } = await this.giphy.trending();
+    return await this.comparisonWithLikes(data);
+  }
+
+  async selectIds(scope: [string] | string[]): Promise<[]> {
+    const { data } = await this.giphy.id(scope);
+    return await this.comparisonWithLikes(data);
+  }
+
+  async selectByTag(tag: string): Promise<[]> {
+    const { data } = await this.giphy.search(tag);
+    return await this.comparisonWithLikes(data);
   }
 }
